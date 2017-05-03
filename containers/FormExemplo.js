9bf6356e5364a69e6557b0856d3a71908e0ad077
vini1889 <vini1889@gmail.com>
2017-05-02 23:15:17 -0300

    commit

--- containers/FormExemplo.js
+++ containers/FormExemplo.js
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
 
+import {Button, Panel} from '../components/ui';
+import {Row, Col, Page} from '../components/ui/Layout';
+import Input from '../components/ui/Input';
+
  class FormExemplo extends Component {
 
-    render() {
+
+    testaEvento(event){
+        alert('dsada');
+        console.log(event);
+    }
+
+
+     render() {
 
         return (
-            <div>dadsa</div>
+            <Page>
+                <Row>
+                    <Col size={6}>
+                        <Panel title="Simple">
+                            <form role="form">
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <Input placeholder="email" />
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <Input placeholder="password" errorMessage="required" format="password" />
+                                </div>
+                                <div className="checkbox">
+                                    <label> <input type="checkbox" /> Remember me </label>
+                                </div>
+                                <Button label="login" color="btn-info" />
+                            </form>
+                        </Panel>
+                    </Col>
+                </Row>
+            </Page>
+
         )
 
     }
